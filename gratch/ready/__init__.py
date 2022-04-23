be166e5ec0c813f20e7f4210dc5f01c88496f13c
larryw3i <larryw3i@163.com>
2022-04-23 20:25:02 +0800

    init

---
+++ gratch/ready/__init__.py
@@ -0,0 +1,14 @@
+import importlib
+import os
+import pickle
+import subprocess
+import threading
+import webbrowser
+from functools import partial
+from itertools import zip_longest
+
+
+
+def go():
+    print('Hello!')
+    pass
