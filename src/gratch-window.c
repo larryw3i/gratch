0e3d5906c6c229e59711656bbe32fd3f53737084
larryw3i <larryw3i@163.com>
2022-05-15 15:42:21 +0800

    zz

--- src/gratch-window.c
+++ src/gratch-window.c
@@ -3,30 +3,29 @@
  * Copyright 2022 larryw3i
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
-#include "gratch-window.h"
 #include "gratch-config.h"
-#include <glib/gi18n.h>
+#include "gratch-window.h"
 
 struct _GratchWindow
 {
-    GtkApplicationWindow parent_instance;
+  GtkApplicationWindow  parent_instance;
 
-    /* Template widgets */
-    GtkHeaderBar *header_bar;
-    GtkLabel *label;
+  /* Template widgets */
+  GtkHeaderBar        *header_bar;
+  GtkLabel            *label;
 };
 
 G_DEFINE_TYPE (GratchWindow, gratch_window, GTK_TYPE_APPLICATION_WINDOW)
@@ -34,19 +33,15 @@ G_DEFINE_TYPE (GratchWindow, gratch_window, GTK_TYPE_APPLICATION_WINDOW)
 static void
 gratch_window_class_init (GratchWindowClass *klass)
 {
-    GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
+  GtkWidgetClass *widget_class = GTK_WIDGET_CLASS (klass);
 
-    gtk_widget_class_set_template_from_resource (
-        widget_class, "/fun/larrywei/gratch/gratch-window.ui");
-    gtk_widget_class_bind_template_child (widget_class, GratchWindow,
-                                          header_bar);
-    gtk_widget_class_bind_template_child (widget_class, GratchWindow, label);
-    // gtk_label_set_text (label, "Hello, World!");
+  gtk_widget_class_set_template_from_resource (widget_class, "/fun/larryw3i/gratch/gratch-window.ui");
+  gtk_widget_class_bind_template_child (widget_class, GratchWindow, header_bar);
+  gtk_widget_class_bind_template_child (widget_class, GratchWindow, label);
 }
 
 static void
 gratch_window_init (GratchWindow *self)
 {
-    gtk_widget_init_template (GTK_WIDGET (self));
-    // gtk_label_set_text (label, "Hello, World!");
+  gtk_widget_init_template (GTK_WIDGET (self));
 }
--- src/main.c
+++ src/main.c
@@ -3,57 +3,54 @@
  * Copyright 2022 larryw3i
  *
  * This program is free software: you can redistribute it and/or modify
- * it under the terms of the GNU Lesser General Public License as published by
+ * it under the terms of the GNU General Public License as published by
  * the Free Software Foundation, either version 3 of the License, or
  * (at your option) any later version.
  *
  * This program is distributed in the hope that it will be useful,
  * but WITHOUT ANY WARRANTY; without even the implied warranty of
  * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
- * GNU Lesser General Public License for more details.
+ * GNU General Public License for more details.
  *
- * You should have received a copy of the GNU Lesser General Public License
+ * You should have received a copy of the GNU General Public License
  * along with this program.  If not, see <http://www.gnu.org/licenses/>.
  */
 
 #include <glib/gi18n.h>
 
-#include "gratch-application.h"
 #include "gratch-config.h"
+#include "gratch-application.h"
 
 int
-main (int argc, char *argv[])
+main (int   argc,
+      char *argv[])
 {
-    g_autoptr (GratchApplication) app = NULL;
-    int ret;
+	g_autoptr(GratchApplication) app = NULL;
+	int ret;
 
-    /* Set up gettext translations */
-    bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
-    bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
-    textdomain (GETTEXT_PACKAGE);
+	/* Set up gettext translations */
+	bindtextdomain (GETTEXT_PACKAGE, LOCALEDIR);
+	bind_textdomain_codeset (GETTEXT_PACKAGE, "UTF-8");
+	textdomain (GETTEXT_PACKAGE);
 
-    /*
-     * Create a new GtkApplication. The application manages our main loop,
-     * application windows, integration with the window manager/compositor, and
-     * desktop features such as file opening and single-instance applications.
-     */
-    app = gratch_application_new ("fun.larrywei.gratch",
-                                  G_APPLICATION_FLAGS_NONE);
+	/*
+	 * Create a new GtkApplication. The application manages our main loop,
+	 * application windows, integration with the window manager/compositor, and
+	 * desktop features such as file opening and single-instance applications.
+	 */
+	app = gratch_application_new ("fun.larryw3i.gratch", G_APPLICATION_FLAGS_NONE);
 
-    g_print(_("Hello, World!"));
+	/*
+	 * Run the application. This function will block until the application
+	 * exits. Upon return, we have our exit code to return to the shell. (This
+	 * is the code you see when you do `echo $?` after running a command in a
+	 * terminal.
+	 *
+	 * Since GtkApplication inherits from GApplication, we use the parent class
+	 * method "run". But we need to cast, which is what the "G_APPLICATION()"
+	 * macro does.
+	 */
+	ret = g_application_run (G_APPLICATION (app), argc, argv);
 
-    /*
-     * Run the application. This function will block until the application
-     * exits. Upon return, we have our exit code to return to the shell. (This
-     * is the code you see when you do `echo $?` after running a command in a
-     * terminal.
-     *
-     * Since GtkApplication inherits from GApplication, we use the parent class
-     * method "run". But we need to cast, which is what the "G_APPLICATION()"
-     * macro does.
-     */
-    ret = g_application_run (G_APPLICATION (app), argc, argv);
-
-
-    return ret;
+	return ret;
 }
